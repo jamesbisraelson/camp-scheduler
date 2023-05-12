a7be153f24ad6c46f580f2e296f83412dc3d8af5
Christian Morrell <christian.morrell@breathebiomedical.com>
2023-05-11 21:33:33 -0300

    Update Stores
    Variable names in stores.js did not match updates from other branches.
    
    Added variables to store scheduled activities and maintenance jobs.

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,4 +1,6 @@
 import { writable } from "svelte/store";
 
 export const activitiesData = writable(null);
-export const scheduleData = writable(null);
+export const maintenanceData = writable(null);
+export const scheduledActivitiesData = writable(null);
+export const scheduledMaintenanceData = writable(null);
