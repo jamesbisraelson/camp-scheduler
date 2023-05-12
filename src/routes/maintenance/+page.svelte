ae1e6257ddd7ab43a65245a8725e4dc94681917e
Christian Morrell <christian.morrell@breathebiomedical.com>
2023-05-11 21:35:26 -0300

    Maintenance Page Table Layout
    Maintenance page required the ability to add and delete maintenance
    jobs.
    
    Created table layout to add and delete maintenance jobs using
    an HTML form. Fields can be changed by the user similar to the
    Activities page.

--- src/routes/maintenance/+page.svelte
+++ src/routes/maintenance/+page.svelte
@@ -2,6 +2,62 @@
   <title>Maintenance</title>
 </svelte:head>
 
+<script>
+  import { maintenanceData } from '../../lib/stores.js';
+
+  class MaintenanceJob {
+    constructor(name, startTime) {
+      this.name = name;
+      this.startTime;
+    }
+  }
+  
+  const columns = ["Job Name", "Start Time"];   // columns for table
+  let jobs = $maintenanceData || [new MaintenanceJob()];  // current possible jobs
+  
+  function saveMaintenanceData() {
+    $maintenanceData = jobs;
+  }
+  
+  function deleteJob(job) {
+    // Remove job from array of jobs
+    jobs.splice(jobs.indexOf(job), 1);
+		jobs = jobs;  // force page to update immediately
+  }
+  
+  function addJob(job) {
+    // Add job to array of jobs
+    jobs.push(job)
+    jobs = jobs;  // force page to update immediately
+  }
+  
+
+</script>
+
 <main>
   <h1>Maintenance</h1>
+  <form on:submit|preventDefault={saveMaintenanceData}>
+    <table>
+      <thead>
+        <tr>
+          {#each columns as column}
+          <!--Create a column headers in table-->
+            <th>{column}</th>
+          {/each}
+        </tr>
+      </thead>
+      <tbody>
+        {#each jobs as job}
+          <!--Create a job input for every saved maintenance job-->
+          <tr>
+            <td><input on:change={saveMaintenanceData} type="text"></td>
+            <td><input on:change={saveMaintenanceData} type="time"></td>
+            <td><button on:click={deleteJob}>x</button></td>
+          </tr>
+        {/each}
+        <!--Create button to add job to list of stored maintenance jobs-->
+        <button on:click={addJob}>Add</button>
+      </tbody>
+    </table>
+  </form>
 </main>
