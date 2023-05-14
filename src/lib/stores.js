b2cd6fd2eb8ccfd9d33e5aa9f96c8b0daf02da13
Christian Morrell <christian.morrell@breathebiomedical.com>
2023-05-13 21:27:21 -0300

    Staff Data Store Fix
    Staff page referenced a store variable that did not exist in stores.js.
    
    Added staffData store variable in stores.js.

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,5 +1,6 @@
 import { writable } from "svelte/store";
 
+export const staffData = writable(null);
 export const activitiesData = writable(null);
 export const maintenanceData = writable(null);
 export const scheduledActivitiesData = writable(null);
