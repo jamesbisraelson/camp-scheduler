ce601b7d72d1702ff2476dd2a48a7a1e2f140b90
Christian Morrell <christian.morrell@breathebiomedical.com>
2023-05-14 23:10:23 -0300

    Convert Time String to Date
    Added function to TimeSlot class that converts a string to a
    Date object.

--- src/lib/scheduling-algorithm.js
+++ src/lib/scheduling-algorithm.js
@@ -1,12 +1,28 @@
-class TimeSlot {
+export class TimeSlot {
     // Class to handle a generic time slot. This can be an activity session, or just a time slot for a job
     constructor(startTime, endTime) {
-        // Start time and end time should be passed in as Date objects
+        // Start time and end time should be passed in as strings (e.g., '12:30')
         this.startTime = startTime;
         this.endTime = endTime;
+        console.log(startTime);
+    }
+
+    timeStringToDate(time) {
+        // Pass in time as a string and return a Date object
+        const timeArray = time.split(':');  // split date string into hours and minutes
+        let date = new Date();
+        date.setUTCHours(timeArray[0], timeArray[1], 0, 0);     // don't care about seconds and milliseconds, set them to 0
+        return date;
     }
 }
 
-var d = new Date();
+// Tests to understand Date object
+let d = new Date();
 d.setUTCHours(0,0,0,0);
 console.log(d);
+
+let timeString = '12:30';
+let timeArray = timeString.split(':');
+let date = new Date();
+date.setUTCHours(timeArray[0], timeArray[1], 0, 0);
+console.log(date);
